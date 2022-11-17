26d47383c83d9f7ef069bfcd84577d3379e6bb2e
huan00 <96999144+huan00@users.noreply.github.com>
2022-11-17 16:09:17 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const cursor_trail = document.getElementById('cursor-trail')
+const container = document.getElementById('container')
+
+window.addEventListener('mouseover', (e) => {
+  const x = e.clientX + 'px'
+  const y = e.clientY + 'px'
+  cursor_trail.style.left = x
+  cursor_trail.style.top = y
+})
