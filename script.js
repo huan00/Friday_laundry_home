1a0e35bbc20fb948b933ac16bcea59f8688a43a3
huan00 <96999144+huan00@users.noreply.github.com>
2022-11-24 19:34:05 -0500

    add animation on spin labs

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const container = document.getElementById('hero')
 const spin_labs = document.getElementById('spin-labs')
 const inner_orbit = document.querySelector('.inner-orbit')
 const outer_orbit = document.querySelector('.outer-orbit')
+const spin_line = document.querySelector('.spin-labs-line')
 
 window.addEventListener('mouseover', (e) => {
   const x = e.clientX + 'px'
@@ -16,10 +17,13 @@ spin_labs.addEventListener('click', () => {
   if (!animation_pause) {
     inner_orbit.style.animationPlayState = 'paused'
     outer_orbit.style.animationPlayState = 'paused'
+    spin_line.style.animationPlayState = 'paused'
+
     animation_pause = true
   } else {
     inner_orbit.style.animationPlayState = 'running'
     outer_orbit.style.animationPlayState = 'running'
+    spin_line.style.animationPlayState = 'running'
     animation_pause = false
   }
 })
