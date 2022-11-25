0f5826aeaae92599d8d1c0881b9e386e246317b4
huan00 <96999144+huan00@users.noreply.github.com>
2022-11-24 19:24:08 -0500

    add animation

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const cursor_trail = document.getElementById('cursor-trail')
 const container = document.getElementById('hero')
+const spin_labs = document.getElementById('spin-labs')
+const inner_orbit = document.querySelector('.inner-orbit')
+const outer_orbit = document.querySelector('.outer-orbit')
 
 window.addEventListener('mouseover', (e) => {
   const x = e.clientX + 'px'
   const y = e.clientY + 'px'
   cursor_trail.style.left = x
   cursor_trail.style.top = y
+})
+
+let animation_pause = false
+spin_labs.addEventListener('click', () => {
+  if (!animation_pause) {
+    inner_orbit.style.animationPlayState = 'paused'
+    outer_orbit.style.animationPlayState = 'paused'
+    animation_pause = true
+  } else {
+    inner_orbit.style.animationPlayState = 'running'
+    outer_orbit.style.animationPlayState = 'running'
+    animation_pause = false
+  }
 })
